f024696750c00e603d4c6961043cdd1f7f4ac2fa
francescapetra <francescalaurapetra@gmail.com>
2021-05-12 14:35:14 +0200

    creazione cartella

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php 
+
+    class Product{
+        
+        public $name;
+        public $color;
+        public $description;
+        public $price;
+        
+        public function __construct($_name, $_color, $_description, $_price){
+            $this->name = $_name;
+            $this->color = $_color;
+            $this->description = $_description;
+            $this->price = $_price;
+
+
+
+        }
+        // public function getAuthor(){
+        //     return $this->name;
+        // }  
+    }
+    // una istanza 1
+       
+        
+      
+
+
+    // una istanza 2
+        
+
+      
+        
+
+    // $array = [
+    //     $promo,
+    //     $article,
+    // ];
+
+
+        // var_dump($article);
+        // echo $promo-> 
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+
+    <body>
+ 
+    </body>
+</html>
