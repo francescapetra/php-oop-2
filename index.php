a100096ff7c4b7dc617c7bdd4cabe168b3ac92bf
francescapetra <francescalaurapetra@gmail.com>
2021-05-12 19:41:41 +0200

    fixed

--- index.php
+++ index.php
@@ -1,127 +1,129 @@
 <?php
-    $user1 = new Seller("Ufficio Tech");
+$user1 = new Seller("Ufficio Tech");
 
-    $user2 = new Seller("Peccato Moda");
+$user2 = new Seller("Peccato Moda");
 
-    class Product
+class Product
+{
+
+    public $name;
+    public $color;
+    public $description;
+    public $price;
+    protected $code;
+    public $vendor;
+
+    public function __construct($_name, $_color, $_description, $_price, $_code, Seller $_vendor)
     {
-
-        public $name;
-        public $color;
-        public $description;
-        public $price;
-        protected $code;
-        public $vendor;
-
-        public function __construct($_name, $_color, $_description, $_price, $_code, Seller $_vendor)
-        {
-            $this->name = $_name;
-            $this->color = $_color;
-            $this->description = $_description;
-            $this->price = $_price;
-            $this->code = $_code;
-            $this->vendor = $_vendor;
-        }
-        public function getCode()
-        {
-            return $this->code;
-        }
+        $this->name = $_name;
+        $this->color = $_color;
+        $this->description = $_description;
+        $this->price = $_price;
+        $this->code = $_code;
+        $this->vendor = $_vendor;
     }
-    class Hightech extends Product
+    public function getCode()
     {
-
-        public $power_source;
-        public $assurance;
-        public $compatibility;
+        return $this->code;
     }
-    $usb = new Hightech("DSK", "Black", "Usb pen", "15$", "65758", $user1);
-    $usb->power_source = 'Charge';
-    $usb->assurance = '2 years';
-    $usb->compatibility = 'PC';
+}
+class Hightech extends Product
+{
 
-    var_dump($usb);
+    public $power_source;
+    public $assurance;
+    public $compatibility;
+}
+$usb = new Hightech("DSK", "Black", "Usb pen", "15$", "65758", $user1);
+$usb->power_source = 'Charge';
+$usb->assurance = '2 years';
+$usb->compatibility = 'PC';
+
+var_dump($usb);
 
 
-    class Clothes extends Product
+class Clothes extends Product
+{
+    public $size;
+    public $tissue;
+    public $season;
+
+    public function __construct($_name, $_color, $_description, $_price, $_code, $_size, $_tissue, $_season, Seller $_vendor)
     {
-        public $size;
-        public $tissue;
-        public $season;
-
-        public function __construct($_name, $_color, $_description, $_price, $_code, $_size, $_tissue, $_season, Seller $_vendor)
-        {
-            parent::__construct($_name, $_color, $_description, $_price, $_code, $_vendor);
-            $this->size = $_size;
-            $this->tissue = $_tissue;
-            $this->season = $_season;
-        }
+        parent::__construct($_name, $_color, $_description, $_price, $_code, $_vendor);
+        $this->size = $_size;
+        $this->tissue = $_tissue;
+        $this->season = $_season;
     }
-    $coat = new Clothes("Teddy Coat", "Camel", "Warm Lapin coat", "250$", "78908", "42EU", "Cashmere and Lapin", "Winter", $user2);
+}
+$coat = new Clothes("Teddy Coat", "Camel", "Warm Lapin coat", "250$", "78908", "42EU", "Cashmere and Lapin", "Winter", $user2);
 
-    class Seller
+class Seller
+{
+    public function __construct($_userSeller)
     {
-        public function __construct($_userSeller)
-        {
-            $this->userSeller = $_userSeller;
-        }
+        $this->userSeller = $_userSeller;
     }
+}
 
-    var_dump($coat);
+var_dump($coat);
 
-    $preview = [
-        $usb,
-        $coat,
-    ];
+$preview = [
+    $usb,
+    $coat,
+];
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Product</title>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product</title>
 
-    <body>
-        <h1>E-commerce Products</h1>
-        <div>
-            <h2>section->> HIGH-TECH</h2>
-            <p><strong>Nome prodotto:</strong> <?php echo $usb->vendor->userSeller; ?></p>
-            <p><strong>Colore:</strong> <?php echo $usb->color; ?></p>
-            <p><strong>Descrizione:</strong> <?php echo $usb->description; ?></p>
-            <p><strong>Prezzo:</strong> <?php echo $usb->price; ?></p>
-            <p><strong>Alimentazione:</strong> <?php echo $usb->power_source; ?></p>
-            <p><strong>Garanzia:</strong> <?php echo $usb->assurance; ?></p>
-            <p><strong>Dispositivo compatibile:</strong> <?php echo $usb->compatibility; ?></p>
-            <p><strong>Codice prodotto:</strong> <?php echo $usb->getCode(); ?></p>
-            <!-- */<p><strong>Venduto da:</strong> <?php 
-            // echo $user1->seller;
-             ?>/* -->
-            </p>
-        </div>
-        <div>
-            <h2>section->> CLOTHES</h2>
-            <p><strong>Nome prodotto:</strong> <?php echo $coat->vendor->userSeller; ?></p>
-            <p><strong>Colore:</strong> <?php echo $coat->color; ?></p>
-            <p><strong>Descrizione:</strong> <?php echo $coat->description; ?></p>
-            <p><strong>Prezzo:</strong> <?php echo $coat->price; ?></p>
-            <p><strong>Alimentazione:</strong> <?php echo $coat->size; ?></p>
-            <p><strong>Garanzia:</strong> <?php echo $coat->tissue; ?></p>
-            <p><strong>Dispositivo compatibile:</strong> <?php echo $coat->season; ?></p>
-            <p><strong>Codice prodotto:</strong> <?php echo $coat->getCode(); ?></p>
-        </div>
-        <div>
-            <h2>section->> BEST SELLER</h2>
-            <?php foreach ($preview as $post) { ?>
-                <strong>Prodotto Must Have: </strong>
-                <a href="#">
-                    <p><?php echo $post->name; ?></p>
-                </a>
-                <p><strong>Prezzo: </strong><?php echo $post->price; ?></p>
-            <?php } ?>
-        </div>
-    </body>
+<body>
+    <h1>E-commerce Products</h1>
+    <div>
+        <h2>section->> HIGH-TECH</h2>
+        <p><strong>Nome prodotto:</strong> <?php echo $usb->name; ?></p>
+        <p><strong>Colore:</strong> <?php echo $usb->color; ?></p>
+        <p><strong>Descrizione:</strong> <?php echo $usb->description; ?></p>
+        <p><strong>Prezzo:</strong> <?php echo $usb->price; ?></p>
+        <p><strong>Alimentazione:</strong> <?php echo $usb->power_source; ?></p>
+        <p><strong>Garanzia:</strong> <?php echo $usb->assurance; ?></p>
+        <p><strong>Dispositivo compatibile:</strong> <?php echo $usb->compatibility; ?></p>
+        <p><strong>Nome prodotto:</strong> <?php echo $usb->vendor->userSeller; ?></p>
+        <p><strong>Venduto da:</strong> <?php echo $usb->getCode(); ?></p>
+        <!-- */<p><strong>Venduto da:</strong> <?php
+                                                // echo $user1->seller;
+                                                ?>/* -->
+        </p>
+    </div>
+    <div>
+        <h2>section->> CLOTHES</h2>
+        <p><strong>Nome prodotto:</strong> <?php echo $coat->name; ?></p>
+        <p><strong>Colore:</strong> <?php echo $coat->color; ?></p>
+        <p><strong>Descrizione:</strong> <?php echo $coat->description; ?></p>
+        <p><strong>Prezzo:</strong> <?php echo $coat->price; ?></p>
+        <p><strong>Alimentazione:</strong> <?php echo $coat->size; ?></p>
+        <p><strong>Garanzia:</strong> <?php echo $coat->tissue; ?></p>
+        <p><strong>Dispositivo compatibile:</strong> <?php echo $coat->season; ?></p>
+        <p><strong>Codice prodotto:</strong> <?php echo $coat->getCode(); ?></p>
+        <p><strong>Venduto da:</strong> <?php echo $coat->vendor->userSeller; ?></p>
+    </div>
+    <div>
+        <h2>section->> BEST SELLER</h2>
+        <?php foreach ($preview as $post) { ?>
+            <strong>Prodotto Must Have: </strong>
+            <a href="#">
+                <p><?php echo $post->name; ?></p>
+            </a>
+            <p><strong>Prezzo: </strong><?php echo $post->price; ?></p>
+        <?php } ?>
+    </div>
+</body>
 
 </html>
